31930f6668e1cf151d005fe6e560a2e255977a98
Yulany Munevar <andra_852@hotmail.com>
2023-07-12 10:47:14 -0500

    adding more expamples to understand javascript theory

--- hoisting.js
+++ hoisting.js
@@ -10,9 +10,30 @@ function saludar() {
   console.log('¡Hola!');
 }
 //////////////////////////7
-saludar(); // Error: TypeError: saludar is not a function
-const saludar = function() {
-  console.log('¡Hola!');
+// saludar(); // Error: TypeError: saludar is not a function
+const saludar2 = function() {
+  console.log('¡Holi!');
 };
-saludar(); // Imprime: ¡Hola!
+saludar2(); // Imprime: ¡Hola!
 
+///////
+for(var i =0 ; i<5; i++){
+  setTimeout(()=>console.log(i), i * 1000) // print 5 , 5 times every 5 seconds
+
+}
+
+////////////////
+const list = ["A", "B", "C"];
+
+function action(element, index) {
+  console.log("i=", index, "list=", element);
+}
+
+list.forEach(action);
+
+/////////
+setTimeout(() => {
+  console.log("Código asíncrono.");
+}, 2000);
+
+console.log("Código síncrono.");
