d0088d1c93baf51de7015037743dd3535aa9d110
Yulany Munevar <andra_852@hotmail.com>
2023-08-07 13:44:42 -0500

    exercise 2

--- codeWars2.js
+++ codeWars2.js
@@ -20,9 +20,37 @@ function removeSmallest(numbers) {
 
   return numbers.filter((value, index) => index !== indexOfMinValue);
 
-;
-
 }
 console.log(removeSmallest([1,2,3,4,5]))
 console.log(removeSmallest( [2,2,1,2,1]))
 console.log(removeSmallest([2]))
+
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+  if(p1==="scissors" && p2==="paper" ||
+  p1==="paper" && p2==="rock" ||
+   p1==="rock" && p2==="scissors"  ){
+   return "Player 1 won!"
+  }else if(p1===p2){
+
+    return  "Draw!"
+
+  }
+
+  else {
+    return "Player 2 won!"
+  }
+
+};
+
+console.log(rps("scissors","paper"))
+console.log(rps("scissors","rock"))
+console.log(rps("rock","rock"))
