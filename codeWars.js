154458fd5b24567ad5a7b52eea7223ac66fe0d7c
Yulany Munevar <andra_852@hotmail.com>
2023-09-21 20:55:12 -0500

    training with algoritms

--- codeWars.js
+++ codeWars.js
@@ -54,3 +54,38 @@ function count (string) {
   });
   return count;
 }
+
+////////
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  let suma = 0
+  classPoints.map((points)=>
+      suma+=points/classPoints.length
+  )
+  if(suma>yourPoints){
+    return false
+  }else {
+    return true
+  }
+
+}
+
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75))
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9))
+
+
+// A hero is on his way to the castle to complete his mission.
+//  However, he's been told that the castle is surrounded with a couple of p
+//  owerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea
+//  how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and
+//  move forward to fight another specific given number of dragons, will he survive?
+
+// Return true if yes, false otherwise :)
+
+function hero(bullets, dragons){
+  //Get Coding!
+  return bullets >= dragons*2 ? true :false
+
+  }
+
+  console.log(hero(10, 5))
